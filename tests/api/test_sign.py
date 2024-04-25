1b59ec807e6fa357b93a9df3d4492fd195d7d257
Paz Bazak <yolobazak1@gmail.com>
2024-04-26 00:46:36 +0300

    added blockchain argument to request

--- tests/api/test_sign.py
+++ tests/api/test_sign.py
@@ -1,4 +1,5 @@
 import pytest
+from blockchain_dal.consts import SupportedBlockchains
 
 from tests.base import BaseTest
 
@@ -8,14 +9,18 @@ class TestSignRouter(BaseTest):
     @pytest.mark.parametrize(
         "payload",
         [
-            {"hash": "8e60a80b015666bd707fac8a6b3b1f8d298b171b63d72a5fed44ccd5323035e3"},
-            {"metadata": {"item_id": "1234"}},
+            {
+                "hash": "8e60a80b015666bd707fac8a6b3b1f8d298b171b63d72a5fed44ccd5323035e3",
+                "blockchain": SupportedBlockchains.Ethereum.value,
+            },
+            {"metadata": {"item_id": "1234"}, "blockchain": SupportedBlockchains.Ethereum.value},
             {
                 "hash": "8e60a80b015666bd707fac8a6b3b1f8d298b171b63d72a5fed44ccd5323035e3",
                 "metadata": {"item_id": "1234"},
-            }
+                "blockchain": SupportedBlockchains.Ethereum.value,
+            },
         ],
-        ids=["hash only", "metadata only", "metadata and hash"]
+        ids=["hash only", "metadata only", "metadata and hash"],
     )
     def test_successful(self, authenticated_api_key, payload):
         response = self.client.post(
@@ -27,10 +32,21 @@ class TestSignRouter(BaseTest):
         data = response.json()
 
         assert "transaction_id" in data
-        assert data["blockchain"] == "Polygon"
+        assert data["blockchain"] == SupportedBlockchains.Ethereum.value
         assert "timestamp" in data
 
-    @pytest.mark.parametrize("invalid_payload", [{}, {"metadata": "not dict"}], ids=["Empty", "Invalid metadata"])
+    @pytest.mark.parametrize(
+        "invalid_payload",
+        [
+            {},
+            {"metadata": "not dict", "blockchain": SupportedBlockchains.Ethereum.value},
+            {
+                "metadata": {"item_id": "1234"},
+                "hash": "8e60a80b015666bd707fac8a6b3b1f8d298b171b63d72a5fed44ccd5323035e3",
+            },
+        ],
+        ids=["Empty", "Invalid metadata", "Missing blockchain"],
+    )
     def test_validation_error(self, authenticated_api_key, invalid_payload):
         response = self.client.post(
             "/api/latest/sign/", json=invalid_payload, headers={"X-API-Key": str(authenticated_api_key.key)}
