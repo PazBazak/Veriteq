de28f9ca9744e9b1067c7ad3f0ee5fc44888affa
Paz Bazak <yolobazak1@gmail.com>
2024-04-26 01:09:36 +0300

    fixed workflow PYTHONPATH

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,7 +1,14 @@
+import os.path
+from pathlib import Path
+
 import yaml
 
+root_dir = Path(__file__).parent.parent.parent
 
-def load_config(config_path: str = "./../config.yaml"):  # todo
+CONFIG_FILE_NAME = "config.yaml"
+
+
+def load_config(config_path: str = os.path.join(root_dir, CONFIG_FILE_NAME)):
     with open(config_path) as fd:
         config = yaml.safe_load(fd)
 
